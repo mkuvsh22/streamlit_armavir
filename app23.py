bf91d9b0e6cb30830f1290434d08f5ecf7dfd32b
mkuvsh22 <119355048+mkuvsh22@users.noreply.github.com>
2024-07-02 22:00:53 +0300

    Update app23.py

--- app23.py
+++ app23.py
@@ -2,15 +2,49 @@ import streamlit as st
 import requests
 import os
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 
-API = os.getenv("API")
+API_KEY = os.getenv("API_KEY")
 
-# Функция для отправки запроса к модели YandexGPT
-def get_response(user_input):
-    req = {
-        "modelUri": "ds://bt14n3vjsod6g6jksd97",
+def send_prompt(prompt):
+    url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Api-Key {API_KEY}"
+    }
+
+    response = requests.post(url, headers=headers, json=prompt)
+    if response.status_code == 200:
+        response_json = response.json()
+        text = response_json['result']['alternatives'][0]['message']['text']
+        return text
+    elif response.status_code == 429:
+        time.sleep(1)
+        
+        response = requests.post(url, headers=headers, json=prompt)
+        if response.status_code == 200:
+            response_json = response.json()
+            text = response_json['result']['alternatives'][0]['message']['text']
+            return text
+        elif response.status_code == 429:
+            time.sleep(4)
+            response = requests.post(url, headers=headers, json=prompt)
+            if response.status_code == 200:
+                response_json = response.json()
+                text = response_json['result']['alternatives'][0]['message']['text']
+                return text
+            else:
+                return f"Error: {response.status_code}, {response.text}"
+        else:
+            return f"Error: {response.status_code}, {response.text}"
+    else:
+        return f"Error: {response.status_code}, {response.text}"
+
+def send_blueprint(prompt: str):
+    prompt = {
+        "modelUri": "gpt://your_catalog/yandexgpt/latest",
         "completionOptions": {
             "stream": False,
             "temperature": 0.1,
@@ -19,17 +53,17 @@ def get_response(user_input):
         "messages": [
             {
                 "role": "user",
-                "text": f"Напиши рецензию на фильм не менее пяти предложений в котором есть: {user_input}"
+                "text": f"{prompt}"
             }
         ]
     }
-    headers = {
-        "Authorization": f"Bearer {API}",
-        "x-folder-id": "b1gik8r0od91a5kkg895",
-    }
-    response = requests.post("https://llm.api.cloud.yandex.net/foundationModels/v1/completion",
-                             headers=headers, json=req)
-    return response.json()
+    
+    result = send_prompt(prompt)
+    
+    # delay
+    time.sleep(0.05)
+    
+    return result
 
 # Интерфейс Streamlit
 st.markdown(
@@ -56,15 +90,14 @@ button_style = (
 
 if st.button('Рецензия', key='review_button', help='Нажмите, чтобы получить рецензию'):
     if user_input:
-        response = get_response(user_input)
-        if 'result' in response and 'alternatives' in response['result'] and len(response['result']['alternatives']) > 0:
-            instruction = response["result"]["alternatives"][0]["message"]["text"]
+        response = send_blueprint(f"Напиши рецензию на фильм не менее пяти предложений в котором есть: {user_input}")
+        if response.startswith("Error:"):
+            st.write(response)
+        else:
             st.markdown(
-                f'<font face="Fira Mono"><br><br>{instruction}</font>',
+                f'<font face="Fira Mono"><br><br>{response}</font>',
                 unsafe_allow_html=True
             )
-        else:
-            st.write("Ошибка: Некорректный ответ от API.")
     else:
         st.write("Пожалуйста, введите описание фильма.")
 
@@ -83,7 +116,30 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+# Таймер для перезагрузки страницы
+st.markdown(
+    """
+    <script>
+    let timer;
+    function resetTimer() {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            window.location.href = 'https://mkuvsh22.github.io/eternalfilmcritic2/';
+        }, 30000); // 30 секунд
+    }
+    window.onload = resetTimer;
+    window.onmousemove = resetTimer;
+    window.onmousedown = resetTimer; // включите любые события, которые хотите отслеживать
+    window.ontouchstart = resetTimer;
+    window.onclick = resetTimer;
+    window.onkeypress = resetTimer;
+    </script>
+    """,
+    unsafe_allow_html=True
+)
+
 st.markdown('<a href="https://mkuvsh22.github.io/eternalfilmcritic2/" target="_blank" style="color: #008080;">Вернуться на интро</a>', unsafe_allow_html=True)
+
 
 
 
