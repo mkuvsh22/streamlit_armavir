22a748322564a1aaffa26b826b4e1c4eac23f4b0
mkuvsh22 <119355048+mkuvsh22@users.noreply.github.com>
2024-07-08 17:48:19 +0300

    Update app23.py

--- app23.py
+++ app23.py
@@ -116,27 +116,5 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Таймер для перезагрузки страницы
-st.markdown(
-    """
-    <script>
-    let timer;
-    function resetTimer() {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            window.location.href = 'https://mkuvsh22.github.io/eternalfilmcritic2/';
-        }, 30000); // 30 секунд
-    }
-    window.onload = resetTimer;
-    window.onmousemove = resetTimer;
-    window.onmousedown = resetTimer; // включите любые события, которые хотите отслеживать
-    window.ontouchstart = resetTimer;
-    window.onclick = resetTimer;
-    window.onkeypress = resetTimer;
-    </script>
-    """,
-    unsafe_allow_html=True
-)
-
 st.markdown('<a href="https://mkuvsh22.github.io/eternalfilmcritic2/" target="_blank" style="color: #008080;">Вернуться на интро</a>', unsafe_allow_html=True)
 
